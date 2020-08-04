b20f57058ef53ddae9ac5081456c82910f872810
brocahontaz <900304@gmail.com>
2020-08-04 18:46:53 +0200

    Prepared express foundation for the application.

--- src/app.js
+++ src/app.js
@@ -11,8 +11,16 @@ const app = express()
 app.set('view engine', 'hbs')
 
 app.engine('hbs', hbs.express4({
-    defaultLayout: path.join(__dirname, 'views,', 'layouts', 'default'),
+    defaultLayout: path.join(__dirname, 'views', 'layouts', 'default'),
     partialsDir: path.join(__dirname, 'views', 'partials')
 }))
+
+app.set('views', path.join(__dirname, 'views'))
+
+app.use(express.urlencoded({ extended: false }))
+
+app.use('/', express.static(path.join(__dirname, 'public')))
+
+app.use('/', require('./routes/homeRouter'))
 
 app.listen(8080, () => console.log('Testing server at http://localhost:8080'))
---
+++ src/controllers/homeController.js
@@ -0,0 +1,9 @@
+'use strict'
+
+const homeController = {}
+
+homeController.index = (req, res) => {
+    res.render('home/index')
+}
+
+module.exports = homeController
