c793567e579dba182cbe9ae9a0dccc74a2e0068b
brocahontaz <900304@gmail.com>
2020-08-04 16:52:25 +0200

    Setting up project with express, express-hbsand some other initializations.

---
+++ src/app.js
@@ -0,0 +1,18 @@
+'use strict'
+
+require('dotenv').config()
+
+const express = require('express')
+const hbs = require('express-hbs')
+const path = require('path')
+
+const app = express()
+
+app.set('view engine', 'hbs')
+
+app.engine('hbs', hbs.express4({
+    defaultLayout: path.join(__dirname, 'views,', 'layouts', 'default'),
+    partialsDir: path.join(__dirname, 'views', 'partials')
+}))
+
+app.listen(8080, () => console.log('Testing server at http://localhost:8080'))
