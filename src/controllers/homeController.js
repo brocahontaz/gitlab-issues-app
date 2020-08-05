6b08fe0089fa0533b462586eafd6fb2a4ef461f4
brocahontaz <900304@gmail.com>
2020-08-05 13:14:55 +0200

    Added node-fetch to be able to fetch data from the API. Started implementing the controller.

--- src/controllers/homeController.js
+++ src/controllers/homeController.js
@@ -1,8 +1,17 @@
 'use strict'
 
+require('dotenv').config()
+
+const fetch = require('node-fetch')
+
 const homeController = {}
 
+const apiUrl = 'https://gitlab.example.com/api/v4//projects/'+process.env.PROJECT_ID+'/issues?private_token='+process.env.ACCESS_TOKEN
+
 homeController.index = (req, res) => {
+
+    const viewData = {}
+
     res.render('home/index')
 }
 
