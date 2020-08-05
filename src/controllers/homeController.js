ff794c66010fe1e1f35586f369a1d7385ddca788
brocahontaz <900304@gmail.com>
2020-08-05 13:19:16 +0200

    Tested fetch from api, seems to work!

--- src/controllers/homeController.js
+++ src/controllers/homeController.js
@@ -6,9 +6,18 @@ const fetch = require('node-fetch')
 
 const homeController = {}
 
-const apiUrl = 'https://gitlab.example.com/api/v4//projects/'+process.env.PROJECT_ID+'/issues?private_token='+process.env.ACCESS_TOKEN
+const apiUrl = 'https://gitlab.lnu.se/api/v4/projects/'+process.env.PROJECT_ID+'/issues?private_token='+process.env.ACCESS_TOKEN
 
-homeController.index = (req, res) => {
+homeController.index = async (req, res) => {
+
+    try {
+    const testFetch = await fetch(apiUrl)
+
+    console.log(testFetch)
+
+    } catch (err) {
+        console.log(err)
+    }
 
     const viewData = {}
 
